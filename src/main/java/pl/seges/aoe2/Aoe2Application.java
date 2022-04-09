624fa4d6de65404a90cc925bc8ae2a42b96251f7
Marcin Kowalski <raggy2k4@gmail.com>
2022-04-09 09:16:10 +0200

    init commit

---
+++ src/main/java/pl/seges/aoe2/Aoe2Application.java
@@ -0,0 +1,13 @@
+package pl.seges.aoe2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aoe2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aoe2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/seges/aoe2/Aoe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.seges.aoe2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aoe2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
