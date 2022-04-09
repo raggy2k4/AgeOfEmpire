38e9b6a34054770c85a3546463945d1848f3476c
Marcin Kowalski <raggy2k4@gmail.com>
2022-04-09 09:36:35 +0200

    add models

---
+++ src/main/java/pl/seges/aoe2/domain/cost/Cost.java
@@ -0,0 +1,10 @@
+package pl.seges.aoe2.domain.cost;
+
+public class Cost {
+
+    int Wood;
+    int Food;
+    int Stone;
+    int Gold;
+
+}
---
+++ src/main/java/pl/seges/aoe2/domain/unit/Unit.java
@@ -0,0 +1,31 @@
+package pl.seges.aoe2.domain.unit;
+
+import pl.seges.aoe2.domain.cost.Cost;
+
+import java.util.List;
+
+public class Unit {
+
+    int id;
+    String name;
+    String description;
+    String expansion;
+    String age;
+    String created_in;
+    Cost cost;
+    int build_time;
+    int reload_time;
+    int attack_delay;
+    int movement_rate;
+    int line_of_sight;
+    int hit_points;
+    String range;
+    int attack;
+    String armor;
+    List<String> attack_bonus;
+    List<String> armor_bonus;
+    int search_radius;
+    String accuracy;
+    int blast_radius;
+
+}
